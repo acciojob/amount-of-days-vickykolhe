62fe979601f336c8559ad24e3fe76c45bc5cf9c3
Babusha Kolhe <165826953+vickykolhe@users.noreply.github.com>
2025-03-31 23:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if((year%4 ===0 && year%100!== 0) || (year%400===0))
+	{
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
+
+let n=prompt("enter the year");
+alert(daysOfAYear(n));
